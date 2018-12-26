d65a6661d72d55d7b286f475a476783f45a2f9c1
tingshuai <1085370797@qq.com>
2018-12-27 07:23:41 +0800

    1.2.0

--- pages/svgEdit/index.vue
+++ pages/svgEdit/index.vue
@@ -1,6 +1,18 @@
 <template>
   <section class="container">
+    <div class="top">
 
+    </div>
+    <div class="bottom">
+      <section class="left">
+        <ul>
+          <li><i class="iconfont icon-xuanze"></i></li>
+          <li><i class="iconfont icon-xuanze"></i></li>
+        </ul>
+      </section>
+      <section class="center"></section>
+      <section class="right"></section>
+    </div>
   </section>
 </template>
 
@@ -54,6 +66,28 @@ export default {
       .container{
           width: 100%;
           height: 100%;
+          display: flex;
+          flex-direction: column;
+          .top{
+            height: 30px;
+          }
+          .bottom{
+            flex-grow: 1;
+            display: flex;
+            justify-content: space-between;
+            .left{
+              width: 100px;
+              border: 1px solid @borderColor;              
+            }
+            .center{
+              flex-grow: 1;
+              border: 1px solid @borderColor;
+            }
+            .right{
+              width: 150px;
+              border: 1px solid @borderColor;
+            }
+          }
       }
   }
 </style>
