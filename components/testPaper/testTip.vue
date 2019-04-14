b898cdc0fe2da947860f81571f6d69514167f22d
qiaof <qiaof@chinaoly.com>
2019-04-15 07:03:15 +0800

    1.2.8
    textarea组建修改

--- components/testPaper/testTip.vue
+++ components/testPaper/testTip.vue
@@ -2,23 +2,22 @@
             <div class="testMsg">
                 <h4 class="h4">2018年普通高等学校招生全国统一考试</h4>
                 <h2 class="h2">理科数学</h2>
-                <textarea autoHeight class="textMsg">
-注意事项： 
-1．答卷前，考生务必将自己的姓名、准考证号填写在答题卡上。 
-2．作答时，务必将答案写在答题卡上。写在本试卷及草稿纸上无效。 
-3．考试结束后，将本试卷和答题卡一并交回。
-                </textarea>
+                <text-area :rows="rows" :value="textVal" autoHeight minHeight="110px" class="textM"></text-area>
             </div>
 </template>
 <script>
-import './textArea.vue'
+import textArea from './textArea.vue'
 export default {
   components: {
-
+    textArea
   },  
   data () {
     return {
-
+      textVal:`注意事项： 
+1．答卷前，考生务必将自己的姓名、准考证号填写在答题卡上。 
+2．作答时，务必将答案写在答题卡上。写在本试卷及草稿纸上无效。 
+3．考试结束后，将本试卷和答题卡一并交回。`,
+      rows:4
     }
   },
   created(){
--- components/testPaper/textArea.vue
+++ components/testPaper/textArea.vue
@@ -1,9 +1,12 @@
 <template>
-    <textarea v-model="value" :autoHeight="autoHeight" class="textMsg"></textarea>
+    <textarea v-model="textVal" unselectable="on" :rows="rows" :readonly="status" :autoHeight="autoHeight" class="textMsg" @input="inputing" @blur="blur" @dblclick="dblclick"></textarea>
 </template>
 <script>
 export default {
     props:{
+        rows:{
+            default:''
+        },
         value:{
             default:''
         },
@@ -11,23 +14,39 @@ export default {
             default:true
         }
     },
+    computed:{
+        
+    },
     components: {
 
     },  
     data () {
         return {
-
+            status:true,
+            textVal:this.value
         }
     },
     created(){
 
     },
     methods:{
-
+        inputing(val){
+            console.log(val);
+        },
+        dblclick(e){
+            this.$nextTick(()=>{
+                this.status = false;
+            })
+        },
+        blur(e){
+            this.status = true;
+        }
     }
 }
 </script>
 
 <style scoped lang="less">
-
+    .textMsg{
+        user-select:none;
+    }
 </style>
